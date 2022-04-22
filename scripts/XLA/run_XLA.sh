b316f12ae368b64c8268e93e3ac95d0f84706cb5
noobotdj <buildfightertry@gmail.com>
2022-04-22 18:31:48 +0800

    update readme

--- scripts/XLA/run_XLA.sh
+++ scripts/XLA/run_XLA.sh
@@ -5,7 +5,7 @@ echo 'XLA Performance Test on GPU'
 for i in $(cat ./eval_list)
 do
     echo $i
-    ~/python3.8/bin/python3.8 -u tf2xla.py \
+    ~/python3.8/bin/python3.8 -u tf2xla.py -d gpu \
         $i \
         1>./evaluate_result/[$i]\_[NVIDIA_V100]_XLA_B1.output
 done
